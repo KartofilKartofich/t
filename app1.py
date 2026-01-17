bfab485d8e494bacb33aae2ee105b1623feb2eee
KartofilKartofich <kartofil.kartofich@yandex.ru>
2026-01-18 01:19:29 +0300

    upd app1.py

--- app1.py
+++ app1.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from streamlit_image_coordinates import streamlit_image_coordinates
 
+real_code = st.secrets["real_code"]
+
 value = streamlit_image_coordinates("IMG_7195.PNG",
                                     #height=480, 
                                     width=st.secrets["width"],
@@ -8,3 +10,53 @@ value = streamlit_image_coordinates("IMG_7195.PNG",
                                     #use_column_width="auto",
                                     cursor="crosshair")#'always')
 st.write(value)
+
+button_width = 54
+button_height = 33
+st.write(value)
+
+if value:
+    b_row = ""
+    b_col = ""
+
+    if value['x']>53 and value['x']<53+button_width:
+        st.write("147x")
+        b_row = "147x"
+    elif value['x']>121 and value['x']<121+button_width:
+        st.write("2580")
+        b_row = "2580"
+    elif value['x']>189 and value['x']<189+button_width:
+        st.write("369v")
+        b_row = "369v"
+
+    if value['y']>231 and value['y']<231+button_height:
+        st.write("123")
+        b_col = "123"
+    elif value['y']>276 and value['y']<276+button_height:
+        st.write("456")
+        b_col = "456"
+    elif value['y']>321 and value['y']<321+button_height:
+        st.write("789")
+        b_col = "789"
+    elif value['y']>366 and value['y']<366+button_height:
+        st.write("789")
+        b_col = "x0v"
+    
+    if b_row != "" and b_col != "": 
+        letter_set = set(b_row)&set(b_col)
+        st.write(letter_set)
+        for letter in letter_set:
+            if letter == "x":
+                if len(st.session_state["written_code"])>0:
+                    st.session_state["written_code"].pop()
+            elif letter == "v":
+                if "".join(st.session_state["written_code"]) == real_code:
+                    st.write("Верный код")
+                else: 
+                    st.session_state["written_code"] = []
+                    st.write("Неверный код")
+            else:
+                st.session_state["written_code"].append(letter)
+
+st.write(st.session_state["written_code"])
+st.text(len(st.session_state["written_code"])*"*")
