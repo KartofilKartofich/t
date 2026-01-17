7fdb9a9bd554a2c8e6c4398342e71b4b1f246a1c
KartofilKartofich <kartofil.kartofich@yandex.ru>
2026-01-18 00:14:04 +0300

    upd app1.py

--- app1.py
+++ app1.py
@@ -3,7 +3,7 @@ from streamlit_image_coordinates import streamlit_image_coordinates
 
 value = streamlit_image_coordinates("IMG_7195.PNG",
                                     #height=480, 
-                                    width=300,
+                                    width=st.secrets["width"],
                                     #use_column_width="always", # always нельзя использовать, меняются координаты при изменении размера вкладки
                                     #use_column_width="auto",
                                     cursor="crosshair")#'always')
